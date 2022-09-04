808c30bb0b36c927c835910cfc2df4d86d0c5146
Alfiya Paukova <alfiya.paukova92@gmail.com>
2022-09-04 17:05:12 +0300

    add Main

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,16 @@
 public class Main {
+    public static void main(String[] args) {
+        int initialAmount = 100; // начальная сумма
+        int populationAmount = 800 ; // сумма пополнения
+        int totalAmount = initialAmount + populationAmount; // сумма на счету после пополнения
+        int bonusAmount = totalAmount / 100; // начисленный бонус за каждые 100 рублей
+        int totalMany = bonusAmount + totalAmount;
+        if (totalAmount >=1000){
+                System.out.println("Денег на счету " + totalMany);
+        } else {
+            System.out.println("Денег на счету " + totalAmount);
+        }
+
+
+    }
 }
